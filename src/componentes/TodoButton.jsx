85dbdd56655bcf4f06238d05e7cd33b49903b54e
giovanyosorio <giovany.torres.799@gmail.com>
2023-09-20 10:19:50 -0500

    todo app

---
+++ src/componentes/TodoButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/TodoButton.css"
+function TodoButton() {
+  return (
+    <div>
+      <button className='CreateTodoButton'>+</button>
+    </div>
+  )
+}
+export{
+    TodoButton
+}
---
+++ src/componentes/TodoSearch.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/TodoSearch.css"
+ function TodoSearch() {
+  return (
+    <div>
+      <input className="TodoSearch" type="text" placeholder='Todos '/>
+    </div>
+  )
+}
+export {
+    TodoSearch
+}
